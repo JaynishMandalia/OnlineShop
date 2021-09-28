3daa55aec02bb2edc7d01c054de86993f3bc5028
Jaynish Mandalia <63401286+JaynishMandalia@users.noreply.github.com>
2021-09-28 09:26:13 +0530

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Infrastructure;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class PagesController : Controller
+    {
+        private readonly OnlineShopContext context;
+
+        public PagesController(OnlineShopContext context)
+        {
+            this.context = context;
+        }
+
+        //GET request   path => /admin/pages
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
+        }
+        
+        //GET request   path => /admin/pages/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if (page == null)
+                return NotFound();
+
+            return View(page);
+        }
+        //GET request   path => /admin/pages/create
+        public IActionResult Create() => View();
+
+
+        [HttpPost]
+        //POST request   path => /admin/pages/create
+        public async Task<IActionResult> Create(Page page)
+        {
+            if(ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if(slug != null)
+                {
+                    ModelState.AddModelError("", "Title Already Exists.");
+                    return View(page);
+                }
+                context.Add(page);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "The Page has been added!";
+                return RedirectToAction("Index");
+            }
+            return View(page);
+        }
+    }
+}
---
+++ OnlineShop/Infrastructure/OnlineShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Infrastructure
+{
+    public class OnlineShopContext : DbContext
+    {
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> options) : base(options)
+        {
+        }
+        public DbSet<Page> Pages { get; set; }
+    }
+}
