2bc1f863529643fe7ee2e76472e75e6515662355
Jaynish Mandalia <63401286+JaynishMandalia@users.noreply.github.com>
2021-09-28 10:07:48 +0530

    Edit Functionallity is Added

--- OnlineShop/Areas/Admin/Controllers/PagesController.cs
+++ OnlineShop/Areas/Admin/Controllers/PagesController.cs
@@ -40,7 +40,7 @@ namespace OnlineShop.Areas.Admin.Controllers
         public IActionResult Create() => View();
 
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         //POST request   path => /admin/pages/create
         public async Task<IActionResult> Create(Page page)
         {
@@ -51,13 +51,45 @@ namespace OnlineShop.Areas.Admin.Controllers
                 var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
                 if(slug != null)
                 {
-                    ModelState.AddModelError("", "Title Already Exists.");
+                    ModelState.AddModelError("", "Page Already Exists.");
                     return View(page);
                 }
                 context.Add(page);
                 await context.SaveChangesAsync();
                 TempData["Success"] = "The Page has been added!";
                 return RedirectToAction("Index");
+            }
+            return View(page);
+        }
+
+        //GET request   path => /admin/pages/edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            Page page = await context.Pages.FindAsync(id);
+            if (page == null)
+                return NotFound();
+
+            return View(page);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        //POST request   path => /admin/pages/create
+        public async Task<IActionResult> Edit(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Id == 1 ? "home" : page.Title.ToLower().Replace(" ", "-");
+
+                var slug = await context.Pages.Where(x => x.Id != page.Id).FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Page Already Exists.");
+                    return View(page);
+                }
+                context.Update(page);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "The page has been edited!";
+                return RedirectToAction("Edit", new { id = page.Id });
             }
             return View(page);
         }
