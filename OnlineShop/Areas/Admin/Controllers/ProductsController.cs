b918c398f2a74436a62edea2a68bc735abc9c851
Jaynish Mandalia <63401286+JaynishMandalia@users.noreply.github.com>
2021-09-29 21:26:40 +0530

    Backend Part Done

--- OnlineShop/Areas/Admin/Controllers/ProductsController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductsController.cs
@@ -34,7 +34,7 @@ namespace OnlineShop.Areas.Admin.Controllers
 
             ViewBag.PageNumber = p;
             ViewBag.PageRange = pageSize;
-            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Products.Count() / pageSize);   
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Products.Count() / pageSize);
             return View(await products.ToListAsync());
         }
 
@@ -91,5 +91,88 @@ namespace OnlineShop.Areas.Admin.Controllers
 
             return View(product);
         }
+
+        //GET request   path => /admin/pages/edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        //POST request   path => /admin/products/edit/4
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Products.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Page Already Exists.");
+                    return View(product);
+                }
+
+                if (product.ImageUpload != null)
+                {
+                    string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    if (!string.Equals(product.Image, "noimage.png"))
+                    {
+                        string oldImagePath = Path.Combine(uploadDir, product.Image);
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+                    }
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    product.Image = imageName;
+                }
+
+
+                context.Update(product);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "The Product has been edited!";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //GET request   path => /admin/products/delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                TempData["Error"] = "The product doest not exists!!";
+            }
+            else
+            {
+                if (!string.Equals(product.Image, "noimage.png"))
+                {
+                    string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    string oldImagePath = Path.Combine(uploadDir, product.Image);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "The product has been deleted successfully!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
